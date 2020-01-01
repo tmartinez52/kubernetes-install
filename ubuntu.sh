12b0e1c8248f7612c1f0504af59cd80a9a03f695
Timothy Martinez <martinez55tim@gmail.com>
2020-01-01 02:36:46 +0000

    initial commit

---
+++ ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#apt-get update
+
+#apt-get install -y
+#apt-transport-https 
+#ca-certifications
+#curl
+#software-properties-common
+
+#curl -s \
+#https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
