46abb8b76e119e26d8e35abfd5c3483582b7250c
Timothy Martinez <martinez55tim@gmail.com>
2020-01-01 22:26:43 +0000

    adding line 37

--- ubuntu.sh
+++ ubuntu.sh
@@ -34,7 +34,7 @@ cat > /etc/docker/daemon.json <<EOF
 "exec-opts": ["native.cgroupdriver=systemd"],
 "log-driver": "json-file",
 "log-opts": {
-
+  "max-size": "100m"
 },
 "storage-driver": "overlay2"
 }
